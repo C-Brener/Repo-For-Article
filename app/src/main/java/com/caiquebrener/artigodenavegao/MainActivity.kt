01c5558c9832f4ac8b2e7d3ee5604596b807a226
C-Brener <brenercaique0806@gmail.com>
2022-10-13 22:25:16 -0300

    Instanciando NavHostFragment

--- app/src/main/java/com/caiquebrener/artigodenavegao/MainActivity.kt
+++ app/src/main/java/com/caiquebrener/artigodenavegao/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.caiquebrener.artigodenavegao
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.caiquebrener.artigodenavegao.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setupNavigation()
+        setupClickListener()
+    }
+
+    private fun setupClickListener() {
+        binding.navigateForDetails.setOnClickListener {
+            navController.navigate(ListFragmentDirections.actionListFragmentToDetailsFragment())
+        }
+        binding.backForListFragment.setOnClickListener {
+            navController.navigate(DetailsFragmentDirections.actionDetailsFragmentToListFragment())
+        }
+    }
+
+    private fun setupNavigation() {
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
